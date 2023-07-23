2b350177ff911b95be86f0da8048f664013688c5
cjsarasty456 <48922529+cjsarasty456@users.noreply.github.com>
2023-07-23 10:37:44 -0500

    Add files via upload

---
+++ 2023/719/definicionVariable/src/definicionvariable/DefinicionVariable.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package definicionvariable;
+
+/**
+ *
+ * @author caliche
+ */
+public class DefinicionVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        Para definir una variable se indica primero
+        EL tipo dato y luego el nombre variable
+        ejemplo para definir una variable entera
+        int num;
+        */
+        int num;
+        /*
+para asignar un valor en una variable
+        se utiliza =
+*/                
+        num=4;
+        System.out.println(num);
+        System.out.println("num");
+                
+                
+        
+        
+        
+        
+    }
+    
+}
