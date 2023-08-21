4ba341cf90fec9565765eeee0f79c1a6b322f8b1
Carlos Cadena <cjsarasty@gmail.com>
2023-08-21 14:16:04 -0500

    se elimina proyecto prueba

--- kotlin/ejercicios/crud php/app/prueba/app/src/main/java/com/example/prueba/model/producto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.prueba.model
-
-data class producto (
-    var id:String,
-    var nombre:String,
-    var descripcion:String,
-    var precio:String,
-    var cantidad:String
-)
