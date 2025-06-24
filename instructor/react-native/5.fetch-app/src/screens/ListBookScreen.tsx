c002860beb93d6d1ee86806d8fd6b0ba978176d7
carlos julio cadena <cjsarasty@gmail.com>
2025-06-24 10:23:12 -0500

    ajustes minimos y comentarios

--- instructor/react-native/5.fetch-app/src/screens/ListBookScreen.tsx
+++ instructor/react-native/5.fetch-app/src/screens/ListBookScreen.tsx
@@ -1,4 +1,4 @@
-// components/BookList.tsx
+//imports
 import React, { useEffect, useState, useCallback } from "react";
 import {
   View,
@@ -8,25 +8,37 @@ import {
   RefreshControl,
   TouchableOpacity,
 } from "react-native";
-import { getAllBook } from "../api/booksApi";
-import { IBook } from "../api/types/IBook";
-import BookCard from "./../components/BookCard";
 
-import { useNavigation } from "@react-navigation/native";
+//importaciones objetos internos
+//importación para consultar los libros
+import { getAllBook } from "../api/booksApi";
+//importación para el objeto book
+import { IBook } from "../api/types/IBook";
+//importación para traer las tarjetas de libros
+import BookCard from "./../components/BookCard";
+//importaciones para la navegación
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import { BookStackParamsList } from "../navigations/types";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
+//objeto tipado para la navegación y los parametros
 type BookScreenNavigationProp = NativeStackNavigationProp<
   BookStackParamsList,
   "ListBook"
 >;
 
+//componente principal
 const BookList: React.FC = () => {
+  //creación objeto de navegación con el tipado y parametro
   const navigation = useNavigation<BookScreenNavigationProp>();
+  //se crea el objeto que almacena la lista de libros y configura
+  //la actualización de lista cuando se consulta
   const [books, setBooks] = useState<IBook[]>([]);
+  //configuración para generar el refesh haciendo scroll hacia abajo
   const [refreshing, setRefreshing] = useState(false);
 
+  //método que solicita la información de los libros y actualiza el objeto de libros
   const fetchBooks = async () => {
     try {
       const data = await getAllBook();
@@ -35,10 +47,36 @@ const BookList: React.FC = () => {
       console.error("Error al obtener libros:", error);
     }
   };
+  //leer: https://react.dev/reference/react/useEffect
+  /*
+  useEffect es un hook de React que te permite ejecutar lógica secundaria (efectos) 
+  después de que el componente se renderiza. Es útil para:
 
+Llamar APIs
+
+Suscribirse a eventos
+
+Iniciar intervalos o temporizadores
+
+Manipular el DOM (en React web)
+
+Cualquier acción que no deba ejecutarse en cada render
+  */
   useEffect(() => {
     fetchBooks();
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      const intervalId = setInterval(() => {
+        fetchBooks();
+      }, 5000);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, [])
+  );
 
   const onRefresh = useCallback(async () => {
     setRefreshing(true);
